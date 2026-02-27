ef1f9177c0597f2d260fb5ae09f0d2918a259399
leyman0111 <leyman0111@gmail.com>
2026-02-27 12:18:08 +0300

    Initial Commit

---
+++ loco-ios/loco_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  loco_iosApp.swift
+//  loco-ios
+//
+//  Created by Лилия Сулейманова on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct loco_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
